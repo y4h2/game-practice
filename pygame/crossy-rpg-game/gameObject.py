2bedda8494719c39c99c2a6534b78405f3d0633b
yu huang <yuhuang.toronto@gmail.com>
2023-02-20 18:12:38 -0500

    add crossy rpg game

---
+++ pygame/crossy-rpg-game/gameObject.py
@@ -0,0 +1,14 @@
+
+
+import pygame
+
+class GameObject:
+    def __init__(self, x, y, width, height, image_path) -> None:
+        image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(
+            image, (width, height))
+        
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
